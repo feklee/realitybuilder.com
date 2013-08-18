e46e2c70891e1cdd84d4fc09c1254a819d52d259
Felix E. Klee <felix.klee@inka.de>
2013-08-18 23:43:00 +0200

    Make port configurable

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/twitter', routes.twitter);
 app.get('/admin', routes.admin);
 app.post('/admin', routes.verifyAdminPassword);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode",
                 app.address().port, app.settings.env);
 });
